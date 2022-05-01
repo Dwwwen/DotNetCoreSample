5686d6d359c32f9120450f67e54febf37e001f8c
Dayne.W <wend911v1@gmail.com>
2022-05-01 12:49:11 +0800

    Init DDD service structure.

---
+++ src/Sample.Api/Controllers/GoodsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sample.Api.Controllers
+{
+    public class GoodsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
